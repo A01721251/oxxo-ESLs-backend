02b9c1fd207c0dec041b62656b5a1efb25a2dfc2
anun0006 <anun0006@student.monash.edu>
2024-04-15 18:45:26 -0600

    Started csv file upload handling

---
+++ controllers/csv-uploadController.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const db = require('../config/database');
+
+const parseCSVAndUpdateDatabase = (filePath) => {
+    return new Promise((resolve, reject) => {
+        const results = [];
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', (data) => results.push(data))
+            .on('end', () => {
+                // Assuming data fields match database columns
+                results.forEach(async item => {
+                    await db.query('INSERT INTO prices SET ?', item, (error, results) => {
+                        if (error) reject(error);
+                    });
+                });
+                resolve();
+            });
+    });
+};
+
+module.exports = {
+    parseCSVAndUpdateDatabase
+};
